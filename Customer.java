eb19b1d0c05510545ee2676dbeeebab88bec1ee3
Ahir Roy <ahara32@gmail.com>
2019-11-19 23:59:24 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,47 +2,51 @@ package Comp250_Ass1;
 
 public class Customer {
     private String name;
-    private int balance_in_cents;
-    private Basket products_buying = new Basket();
+    private int balance;
+    private Basket basket;
 
-    public Customer(String name, int balance_in_cents){
-            this.name = name;
-            this.balance_in_cents = balance_in_cents;
+
+    public Customer(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+        basket = new Basket();
     }
 
-    public String getName(){
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public String getName() {
         return name;
-
     }
 
-    public int getBalance(){
-        return balance_in_cents;
+    public int getBalance() {
+        return this.balance;
     }
 
-    public Basket getBasket(){
-        return products_buying;
+    public void addToBasket(MarketProduct mp){
+        basket.add(mp);
     }
 
-    public int addFunds(int more_cent){
-        return balance_in_cents + more_cent;
-        //also throw illegal expression, don't know how to do it
+    public boolean removeFromBasket(MarketProduct mp){
+        return basket.remove(mp);
     }
 
-    public void addToBasket(MarketProduct myMarketProduct){
-        this.products_buying.add(myMarketProduct);
-
+    public int addFunds(int amount){
+        if(amount<0){
+            throw new IllegalArgumentException("Negative amount!");
+        }
+         balance = balance + amount;
+        return balance;
     }
-
-    public boolean removeFromBasket(MarketProduct myMarketProduct){
-        return this.products_buying.remove(myMarketProduct);
-    }
-
 
     public String checkOut(){
-        return "safcea";
+        if(balance < basket.getTotalCost()){
+            throw new IllegalStateException();
+        }
+        balance = balance - basket.getTotalCost();
+        String output = basket.toString();
+        basket.clear();
+        return output;
     }
-
-
-
-
 }
